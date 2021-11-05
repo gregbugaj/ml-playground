b7733eb8d2a313f93f76557a90fbbabce871b14e
Greg <gregbugaj@yahoo.com>
2021-11-05 03:52:19 -0500

    Initial commit

--- beam-search/beam_search.py
+++ beam-search/beam_search.py
@@ -1,3 +1,8 @@
+import numpy as np
+
+from numpy import argmax
+from numpy import array
+
 # define a sequence of 10 words over a vocab of 5 words
 data = [[0.1, 0.2, 0.3, 0.4, 0.5],
 		[0.5, 0.4, 0.3, 0.2, 0.1],
@@ -9,4 +14,12 @@ data = [[0.1, 0.2, 0.3, 0.4, 0.5],
 		[0.5, 0.4, 0.3, 0.2, 0.1],
 		[0.1, 0.2, 0.3, 0.4, 0.5],
 		[0.5, 0.4, 0.3, 0.2, 0.1]]
+
 data = array(data)
+
+def greedy_decoder(data):
+    # index for largest probability for each row
+    return [argmax(s) for s in data]
+
+result = greedy_decoder(data)
+print(result)
